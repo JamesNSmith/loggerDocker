dc75c6c945d767406ca08c62136ced209f528c11
James Smith <jamesnsmith@hotmail.co.uk>
2019-05-01 13:59:13 +0100

    environmental variables added

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,12 +27,27 @@ Rails.application.configure do
 
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
+  
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+
+  config.action_mailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+    :address => ENV['MAILER_ADDRESS'],
+    :port => 587,
+    :user_name => ENV['MAILER_USERNAME'], 
+    :password => ENV['MAILER_PASSWORD'], 
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
+  #config.action_mailer.delivery_method = :test
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
